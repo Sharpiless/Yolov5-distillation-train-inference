09003f77824191f465a6ae9d002155484321c6c1
yangziteng <1620842362@qq.com>
2022-10-29 16:28:42 +0800

    2

--- .history/utils/autoanchor_20221029143018.py
+++ .history/utils/autoanchor_20221029143018.py
@@ -4,30 +4,19 @@ import numpy as np
 import torch
 import yaml
 from tqdm import tqdm
-import logging
-logger = logging.getLogger(__name__)
+
 from utils.general import colorstr
-PREFIX = colorstr('AutoAnchor: ')
 
 
 def check_anchor_order(m):
     # Check anchor order against stride order for YOLOv5 Detect() module m, and correct if necessary
-    a = m.anchors.prod(-1).mean(-1).view(-1)  # mean anchor area per output layer
+    a = m.anchor_grid.prod(-1).view(-1)  # anchor area
     da = a[-1] - a[0]  # delta a
     ds = m.stride[-1] - m.stride[0]  # delta s
-    if da and (da.sign() != ds.sign()):  # same order
-        logger.info(f'{PREFIX}Reversing anchor order')
+    if da.sign() != ds.sign():  # same order
+        print('Reversing anchor order')
         m.anchors[:] = m.anchors.flip(0)
-
-# def check_anchor_order(m):
-#     # Check anchor order against stride order for YOLOv5 Detect() module m, and correct if necessary
-#     a = m.anchor_grid.prod(-1).view(-1)  # anchor area
-#     da = a[-1] - a[0]  # delta a
-#     ds = m.stride[-1] - m.stride[0]  # delta s
-#     if da.sign() != ds.sign():  # same order
-#         print('Reversing anchor order')
-#         m.anchors[:] = m.anchors.flip(0)
-#         m.anchor_grid[:] = m.anchor_grid.flip(0)
+        m.anchor_grid[:] = m.anchor_grid.flip(0)
 
 
 def check_anchors(dataset, model, thr=4.0, imgsz=640):
